b4d01ebde3276271570a287ba0edd47eb88cfff3
eg00 <egor@povshok.ru>
2024-03-12 10:10:43 +0400

    Model

---
+++ database/factories/CurrencyRateFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enum\Currency;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CurrencyRate>
+ */
+class CurrencyRateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'currency' => $this->faker->randomElement(Currency::cases()),
+            'value' => $this->faker->randomFloat(5, 0.01, 1000),
+        ];
+    }
+}
